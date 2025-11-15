9e8cb94bfe2f2d5ba3097efcc7bb34a1391de263
andersbor <anbo@zealand.dk>
2025-11-15 10:45:24 +0100

    defensive checks

--- restconsumer.py
+++ restconsumer.py
@@ -1,31 +1,35 @@
 import requests # install this library from the command line: 
 # $ python -m pip install requests
-import json
 
 BASE_URL = "http://localhost:5088/api/aktiehandel"
 
 response = requests.get(BASE_URL)
 print(f"GET status code {response.status_code}")
-print(response.json())
+if response.status_code == 200:
+  print(response.json())
 
 AKTIE_DICTIONARY = {
     "navn": "Mit selskab",
     "antal": 45,
     "pris": 11
 }
-data_json = json.dumps(AKTIE_DICTIONARY)
-HEADERS = {"Content-Type": "application/json"}
-# TODO ... json = ... ej med PUT
-response = requests.post(BASE_URL, data=data_json, headers=HEADERS)
 
+response = requests.post(BASE_URL, json=AKTIE_DICTIONARY)
+# json= automatically converts the dictionary to a JSON string
+# json= automatically sets the Content-Type header to application/json
+ 
 print(f"POST status code {response.status_code}")
-print(response.json())  
+if response.status_code == 201:
+  print(response.json())  
 
-response = requests.delete(BASE_URL + "/1")
+url = BASE_URL + "/1"
+response = requests.delete(url)
 print(f"DELETE status code {response.status_code}")
 if response.status_code == 200:
     print(response.json())
 
-response= requests.put(BASE_URL + "/2", data=data_json, headers=HEADERS)
+url = f"{BASE_URL}/2"
+response= requests.put(url, json=AKTIE_DICTIONARY)
 print(f"PUT status code {response.status_code}")
-print(response.json())
+if response.status_code == 200:
+  print(response.json())
